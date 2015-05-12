6ad1be8588bd2793793cc6b76f314c73132b1c2a
Gideon Dresdner <gideond@gmail.com>
2015-05-13 01:16:23 +0300

    broke the next test

--- test/test_trie.jl
+++ test/test_trie.jl
@@ -4,22 +4,22 @@ using Base.Test
 t=Trie{Char,Int}()
 
 t["amy"]=56
-# t["ann"]=15
-# t["emma"]=30
-# t["rob"]=27
-# t["roger"]=52
+t["ann"]=15
+t["emma"]=30
+t["rob"]=27
+t["roger"]=52
 
-# @test haskey(t, "roger")
-# @test get(t,"rob",nothing) == 27
-# @test sort(keys(t)) == ["amy", "ann", "emma", "rob", "roger"]
-# @test t["rob"] == 27
-# @test sort(keys_with_prefix(t,"ro")) == ["rob", "roger"]
+@test haskey(t, "roger")
+@test get(t,"rob",nothing) == 27
+@test sort(keys(t)) == ["amy", "ann", "emma", "rob", "roger"]
+@test t["rob"] == 27
+@test sort(keys_with_prefix(t,"ro")) == ["rob", "roger"]
 
-# # constructors
-# ks = ["amy", "ann", "emma", "rob", "roger"]
-# vs = [56, 15, 30, 27, 52]
-# kvs = collect(zip(ks, vs))
-# @test typeof(Trie(ks, vs)) == Trie{Int}
+# constructors
+ks = ["amy", "ann", "emma", "rob", "roger"]
+vs = [56, 15, 30, 27, 52]
+kvs = collect(zip(ks, vs))
+@test typeof(Trie(ks, vs)) == Trie{Int}
 # @test typeof(Trie(kvs)) == Trie{Int}
 # @test typeof(Trie(Dict(kvs))) == Trie{Int}
 # @test typeof(Trie(ks)) == Trie{Nothing}
